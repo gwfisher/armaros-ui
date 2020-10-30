c7d15c09b1f890e919c9df6615ed1202e3cbe371
Wesley Fisher <wfisher@vespasian.reality.dataforge.network>
2020-10-29 21:48:49 -0500

    just some more fiddlin'

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,20 @@
-import { AppBar, IconButton } from '@material-ui/core';
+import { Drawer, AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 export default function NavBar() {
     return (
       <AppBar position="static">
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <
-        </IconButton>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu" onClick={ drawerOpen }>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">
+            Armaros
+          </Typography>
+        </Toolbar>
+        <Drawer anchor="left" variant="persistent">
+          
+        </Drawer>
       </AppBar>
-    );
+  );
 }
