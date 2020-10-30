9f5ac2f9ba0983607cd88908430a214417209939
Wesley Fisher <wfisher@vespasian.reality.dataforge.network>
2020-10-29 20:01:16 -0500

    general cleanup

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import { AppBar, IconButton } from '@material-ui/core';
+
+export default function NavBar() {
+    return (
+      <AppBar position="static">
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <
+        </IconButton>
+      </AppBar>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import NavBar from './components/NavBar';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
   </React.StrictMode>,
   document.getElementById('root')
 );
