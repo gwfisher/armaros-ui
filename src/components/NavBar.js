0d71e7b788a62ce37117779820baade2e411c7c1
Wesley Fisher <wfisher@vespasian.reality.dataforge.network>
2020-10-30 13:02:18 -0500

    more work on skeleton

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,23 @@
 import { Drawer, AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import React from 'react';
+import { makeStyles, useTheme} from '@material-ui/core/styles';
+
 
 export default function NavBar() {
+    const classes = useStyles();
+    const theme = useTheme();
+
+    const [open, setOpen] = React.useState(false);
+
+    const drawerOpen = () => {
+      setOpen(true);
+    }
+
+    const drawerClose = () => {
+      setOpen(false);
+    }
+
     return (
       <AppBar position="static">
         <Toolbar>
